659a634ad7ed5705ab4f4ae7e35db7c2adf6c54d
Victor Raposo <vwraposo@gmail.com>
2020-03-24 17:06:55 -0300

    Adds sleep

--- node/middlewares/indexRoutes.ts
+++ node/middlewares/indexRoutes.ts
@@ -1,27 +1,32 @@
 const BROADCASTER_NOTIFICATION = 'broadcaster.notification'
 const PAGE_LIMIT = 50
+
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
+
 const index = async (ctx: ServiceContext) => {
   const { clients: { catalog, events} } = ctx
   let skuIds: number[] = []
   let from = 0
   let to = PAGE_LIMIT - 1
   do {
-    debugger
     const { data } = await catalog.getProductsAndSkuIds(from, to)
     const productIds = Object.keys(data)
     skuIds = productIds.reduce((acc, productId) => {
       const skus = data[productId]
       if (skus.length) {
         const skuId = skus[0]
-        // events.sendEvent('', BROADCASTER_NOTIFICATION, {
-        //   HasStockKeepingUnitModified: true,
-        //   IdSku: skuId,
-        // })
+        events.sendEvent('', BROADCASTER_NOTIFICATION, {
+          HasStockKeepingUnitModified: true,
+          IdSku: skuId,
+        })
+        console.log('SKU EVENT', skuId)
         acc.push(skuId)
       }
       return acc
     }, [] as number[])
-    debugger
+    from += PAGE_LIMIT
+    to += PAGE_LIMIT
+    await sleep(300)
   } while(skuIds.length)
 }
 
