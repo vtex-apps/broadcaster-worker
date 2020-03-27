0f9c95e275114164dd0a8191e88ea724f9f45e7f
Victor Raposo <vwraposo@gmail.com>
2020-03-27 15:28:53 -0300

    More improvements

--- node/middlewares/indexRoutes.ts
+++ node/middlewares/indexRoutes.ts
@@ -1,13 +1,14 @@
 const BROADCASTER_NOTIFICATION = 'broadcaster.notification'
-const PAGE_LIMIT = 50
+const PAGE_LIMIT = 20
 
 const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
 
 const index = async (ctx: ServiceContext) => {
-  const { clients: { catalog, events} } = ctx
+  const { clients: { catalog, events}, vtex: { logger } } = ctx
   let skuIds: number[] = []
   let from = 0
   let to = PAGE_LIMIT - 1
+  let totalProcessedProducts = 0
   do {
     const { data } = await catalog.getProductsAndSkuIds(from, to)
     const productIds = Object.keys(data)
@@ -21,6 +22,7 @@ const index = async (ctx: ServiceContext) => {
           alwaysNotify: true,
         })
         acc.push(skuId)
+        totalProcessedProducts += 1
       }
       return acc
     }, [] as number[])
@@ -28,9 +30,14 @@ const index = async (ctx: ServiceContext) => {
     to += PAGE_LIMIT
     await sleep(500)
   } while(skuIds.length)
+  logger.info(`Indexation of ${totalProcessedProducts} complete`)
 }
 
 export function indexRoutes(ctx: ServiceContext) {
+  if (!ctx.vtex.adminUserAuthToken) {
+    ctx.status = 401
+    return
+  }
   index(ctx)
   ctx.status = 200
 }
