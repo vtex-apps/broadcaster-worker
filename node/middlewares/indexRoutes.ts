598af018eeb865c8b07cea6486fbdab0ae182306
Victor Raposo <vwraposo@gmail.com>
2020-03-31 13:48:19 -0300

    Send events more slowly

--- node/middlewares/indexRoutes.ts
+++ node/middlewares/indexRoutes.ts
@@ -1,11 +1,10 @@
-import { sleep } from "../utils/event"
+import { sleep } from '../utils/event'
 
 const BROADCASTER_NOTIFICATION = 'broadcaster.notification'
 const PAGE_LIMIT = 20
 
-
 const index = async (ctx: ServiceContext) => {
-  const { clients: { catalog, events}, vtex: { logger } } = ctx
+  const { clients: { catalog, events }, vtex: { logger } } = ctx
   let skuIds: number[] = []
   let from = 0
   let to = PAGE_LIMIT - 1
@@ -17,26 +16,32 @@ const index = async (ctx: ServiceContext) => {
       const skus = data[productId]
       if (skus.length) {
         const skuId = skus[0]
-        events.sendEvent('', BROADCASTER_NOTIFICATION, {
-          HasStockKeepingUnitModified: true,
-          IdSku: skuId,
-          alwaysNotify: true,
-        })
         acc.push(skuId)
         totalProcessedProducts += 1
       }
       return acc
     }, [] as number[])
+    for (let skuId in skuIds) {
+      await sleep(300)
+      events.sendEvent('', BROADCASTER_NOTIFICATION, {
+        HasStockKeepingUnitModified: true,
+        IdSku: skuId,
+        alwaysNotify: true,
+      })
+    }
     from += PAGE_LIMIT
     to += PAGE_LIMIT
-    await sleep(500)
+    logger.error(`Done: ${totalProcessedProducts}`)
+    await sleep(1000)
   } while(skuIds.length)
-  logger.info(`Indexation of ${totalProcessedProducts} complete`)
+  logger.error(`Indexation of ${totalProcessedProducts} products complete`)
 }
 
 export function indexRoutes(ctx: ServiceContext) {
-  if (!ctx.vtex.adminUserAuthToken) {
+  const { adminUserAuthToken, logger } = ctx.vtex
+  if (!adminUserAuthToken) {
     ctx.status = 401
+    logger.error(`Missiing adminUserAuth token`)
     return
   }
   index(ctx)
